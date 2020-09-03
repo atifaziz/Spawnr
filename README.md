a7b800e9739d8125060caa544b4bc9374ff6ee4e
Atif Aziz <code@raboof.com>
2020-09-03 21:09:57 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spawnr
+
+Spawnr is .NET Standard library that makes [`Process.Start`][Process.Start]
+simple. It has a functional API for spawning a process and harnessing its
+output as a reactive stream.
+
+  [Process.Start]: [https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.process.start]
---
+++ tests/SpawnOptionsTests.cs
@@ -0,0 +1,45 @@
+namespace Spawnr.Tests
+{
+    using System;
+    using System.Diagnostics;
+    using NUnit.Framework;
+
+    public class SpawnOptionsTests
+    {
+        [Test]
+        public void Create()
+        {
+            var options = SpawnOptions.Create();
+
+            Assert.That(options.Arguments.Count, Is.Zero);
+            Assert.That(options.WorkingDirectory, Is.EqualTo(Environment.CurrentDirectory));
+
+            var env = Environment.GetEnvironmentVariables();
+            Assert.That(options.Environment.Length, Is.EqualTo(env.Count));
+
+            foreach (var e in options.Environment)
+                Assert.That(env[e.Key], Is.EqualTo(e.Value));
+        }
+
+        [Test]
+        public void Update()
+        {
+            var psi = new ProcessStartInfo();
+
+            Assert.That(psi.WorkingDirectory, Is.Empty);
+            Assert.That(psi.ArgumentList, Is.Empty);
+
+            var options = SpawnOptions.Create();
+            options.Update(psi);
+
+            Assert.That(psi.WorkingDirectory, Is.EqualTo(options.WorkingDirectory));
+            Assert.That(psi.ArgumentList, Is.EqualTo(options.Arguments));
+
+            var env = psi.Environment;
+            Assert.That(env.Count, Is.EqualTo(options.Environment.Length));
+
+            foreach (var e in options.Environment)
+                Assert.That(env[e.Key], Is.EqualTo(e.Value));
+        }
+    }
+}
